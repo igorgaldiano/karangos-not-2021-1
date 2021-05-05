ff099e8c105fe6bd7e7815614942d5fe1b34d530
igorgaldiano <igorgaldiano80@gmail.com>
2021-05-05 01:21:44 +0000

    (04/06) Introducao do router e conclusao da barra

--- karangos/src/App.js
+++ karangos/src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-import Button from '@material-ui/core/Button'
+//import Button from '@material-ui/core/Button' 
 import TopBar from './ui/TopBar'
+import {Box} from '@material-ui/core'
+import  FooterBar from './ui/FooterBar'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { createMuiTheme, ThemeProvider,makeStyles } from '@material-ui/core/styles';
 import yellow from '@material-ui/core/colors/yellow';
 import pink from '@material-ui/core/colors/pink';
-
+import KarangosList from './routed/KarangosList'
+import KarangosForm from './routed/KarangosForm'
 const theme = createMuiTheme({
   palette: {
     type: 'dark',
@@ -19,10 +23,53 @@ const theme = createMuiTheme({
   },
 });
 
-function App() {
+const useStyles = makeStyles((theme) =>
+({
+  box: {
+    backgroundColor: theme.palette.background.default,
+    minHeght: '100vh' // 100% da altura da area visivel
+    },
+    routed: {
+      padding: '25px',
+      color: theme.palette.text.primary,
+      fontFamily: theme.typography.fontFamily
+    }
+
+}));
+
+function Main(){
+  const classes = useStyles();
   return (
-    <ThemeProvider theme={theme}>
-      <TopBar/>
+    <Box className = {classes.box}> 
+      <BrowserRouter>
+       
+       <TopBar />
+        <Box id="routed" className={classes.routed}>
+            <Switch>
+               <Route path="/list">
+                 <KarangosList />
+               </Route>
+               <Route path="/new">
+                  <KarangosForm/>
+               </Route>
+
+            </Switch>
+            
+        </Box>
+        <FooterBar />
+      
+      </BrowserRouter>
+     
+    </Box>
+
+  )
+}
+
+function App() {
+ 
+  return ( // 
+    <ThemeProvider theme={theme}> 
+     <Main/>
     </ThemeProvider>
 
   );
---
+++ karangos/src/routed/KarangosList.js
@@ -0,0 +1,6 @@
+export default function KarangosList(){
+    return(
+        <h1>Listagem de Karangos</h1>
+
+    )
+}
